61cff493b1f65cdef18aa3836d66b3ad348def8c
lazySir <969060742@qq.com>
2022-06-13 19:04:55 +0800

    获取商品列表小更新
    1.修改了vuex的语句

--- src/store/search.js
+++ src/store/search.js
@@ -11,25 +11,31 @@ export default {
     },
   },
   actions: {
-    getSearchList(context, payload = {}) {
+    async getSearchList(context, payload = {}) {
       //当前reqGerSearchInfo这个函数在调用获取服务器的时候，至少需要传递一个参数（空对象）
       //params形参：是用户派发action的时候，第二个参数传递过过来的，至少是一个空对象
-      reqGetSearchInfo(payload).then((res) => {
-        if (res.code == 200) {
-          context.commit('GETSEARCHLIST', res.data)
-        }
-      })
+      // reqGetSearchInfo(payload).then((res) => {
+      //   if (res.code == 200) {
+      //     context.commit('GETSEARCHLIST', res.data)
+      //   }
+      // })
+      let result = await reqGetSearchInfo(payload)
+      if (result.code == 200) {
+        context.commit('GETSEARCHLIST', result.data)
+      }
     },
   },
   getters: {
-    goodsList(state){
+    goodsList(state) {
       return state.searchList.goodsList || []
     },
-    attrsList(state){ //平台信息
+    attrsList(state) {
+      //平台信息
       return state.searchList.attrsList || []
     },
-    trademarkList(state){   //商标列表   //品牌
+    trademarkList(state) {
+      //商标列表   //品牌
       return state.searchList.trademarkList || []
-    }
+    },
   },
 }
