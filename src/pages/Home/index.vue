794c382ea4e81a404a290639d26f3c52c2bd20ab
lazySir <969060742@qq.com>
2023-09-17 16:07:21 +0800

    完结
    完结

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,54 +1,47 @@
 <template>
-<div>
-
-  <TypeNav></TypeNav>
-  <ListContainer></ListContainer>
-  <Recommend></Recommend>
-  <Rank></Rank>
-  <Like></Like>
-   <Floor v-for='(floor,index) in floorList' :list='floor' :key="floor.id"></Floor>
-   <Brand></Brand>
-</div>
-
+  <div>
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor v-for="(floor, index) in floorList" :list="floor" :key="floor.id"></Floor>
+    <Brand></Brand>
+  </div>
 </template>
 
 <script>
-
-
 import ListContainer from '@/pages/Home/ListContainer'
 import Recommend from '@/pages/Home/Recommend'
 import Rank from '@/pages/Home/Rank'
 import Like from '@/pages/Home/Like'
 import Floor from '@/pages/Home/Floor'
 import Brand from '@/pages/Home/Brand'
-import {mapActions,mapState} from 'vuex'
+import { mapActions, mapState } from 'vuex'
 export default {
-name:'Home',
-components:{
-  ListContainer,
-Recommend,
-Rank,
-Like,
-Floor,
-Brand
-},
-  methods:{
-    ...mapActions("home",['getFloorList']),
+  name: 'Home',
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
   },
-  computed:{
-    ...mapState('home',['floorList'])
+  methods: {
+    ...mapActions('home', ['getFloorList']),
   },
-  mounted(){
+  computed: {
+    ...mapState('home', ['floorList']),
+  },
+  mounted() {
     //派发action，获取floor组件
     this.getFloorList()
-
-  }
-// created(){
-//  reqCategoryList().then(res=>{console.log(res)})
-// }
+  },
+  // created(){
+  //  reqCategoryList().then(res=>{console.log(res)})
+  // }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
